42e97780778ebeece59b19663d9af421e90b7c4d
ClaraMoon <Claralm2001@proton.me>
2022-07-06 00:01:39 +1000

    Stub everything, start working on basic contract

---
+++ contracts/pixelContract.sol
@@ -0,0 +1,34 @@
+
+pragma solidity >=0.7.0 <0.9.0;
+
+// look at this guide https://www.geeksforgeeks.org/solidity-enums-and-structs/#:~:text=Structs%20in%20Solidity%20allows%20you,and%20imported%20in%20another%20contract.
+
+contract Pixel {
+
+     struct Pixel {
+      uint id;
+      string colour;
+   }
+
+     Pixel pixel1= Pixel(1,"G");
+ 
+
+function buyPixel() public view
+            returns (bytes32 winnerName_)
+    {
+        
+    }
+
+function changePixelColor() public view
+            returns (bytes32 winnerName_)
+    {
+        
+    }
+
+    function getPixelColor() public view
+            returns (bytes32 winnerName_)
+    {
+        
+    }
+
+}
